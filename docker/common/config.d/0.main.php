52da4eb56251377df591e9a334d6b152cce6e43d
Victor Ferreira <vitfera@hotmail.com>
2025-08-05 05:35:07 -0300

    theme RibeiraoDasNeves

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,11 +1,13 @@
 <?php 
 
 return [
-    'app.siteName' => 'Mapa Cultural Ribeirão das Neves',
-    'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
+    'app.siteName' => 'Mapa da Cultura Nevense',
+    'app.siteDescription' => 'O Mapa da Cultura Nevense é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
-    'themes.active' => 'MapasCulturais\Themes\BaseV2',
+    'themes.active' => 'RibeiraoDasNeves',
+
+    'app.lcode' => 'pt_BR',
 
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
---
+++ docker/common/config.d/texts.php
@@ -0,0 +1,17 @@
+<?php
+use \MapasCulturais\i;
+
+return [
+    "text:home-header.title" => i::__("Boas vindas ao Mapa da Cultura Nevense"),
+    "text:home-header.description" => i::__("O Mapa da Cultura Nevense é uma ferramenta de gestão cultural que possibilita o mapeamento colaborativo de agentes culturais, realização de todas as etapas de editais e fomentos, organização de agenda cultural e divulgação de espaços culturais do município de Ribeirão das Neves."),
+    "text:home-entities.title" => i::__("Aqui você encontra informações da cultura de Ribeirão das Neves."),
+    //"text:home-opportunities.description" => i::__("Cadastre-se, participe de editais e oportunidades e concorra aos benefícios."),
+    "text:home-entities.events" => i::__("Você pode pesquisar eventos culturais do município. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente."),
+    "text:home-entities.agents" => i::__("Neste espaço, estão registrados artistas, gestores e produtores: uma rede de atores culturais envolvidos na cena cultural de Ribeirão das Neves. Você pode cadastrar um ou mais agentes (grupos, mestres de cultura, coletivos, bandas, instituições, empresas, etc)."),
+    //"text:home-entities.projects" => i::__("Aqui você encontra leis de fomento, mostras, convocatórias, chamadas públicas e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma."),
+    //"text:home-feature.description" => i::__("Confira os últimos destaques de cada uma das áreas."),
+    "text:home-register.title" => i::__("Faça seu cadastro e colabore com o Mapa da Cultura Nevense!"),
+    "text:home-register.description" => i::__("Colabore com a plataforma livre, colaborativa e interativa de mapeamento do cenário cultural. Este é um instrumento de governança digital que contribui para o aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas públicas culturais promovidas no município de Ribeirão das Neves."),
+    //'text:home-developers.description' => 'Além disso, Mapa Goiano é um software livre, criado em parceria entre a hacklab/, secretarias de cultura, organizações não governamentais, empresas e coletivos que investem na plataforma. Você pode contribuir para o seu desenvolvimento através do GitHub.',
+    //"text:home-map.description" => i::__("Os agentes, os espaços e os eventos cadastrados contam com a geolocalização de seus endereços, encontre-os aqui:"),
+];
